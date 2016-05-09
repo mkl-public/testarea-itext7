d1a90f851da82b8dd24bcd68367efa72cdcccabb
mkl <klink@csi.com>
2016-05-09 12:47:04 +0200

    New ChangeSignatureAppearance test testChangeAppearancesWithName focusing on the stackoverflow question
    How to associate a previous signature in a new signature field
    http://stackoverflow.com/questions/37027579/how-to-associate-a-previous-signature-in-a-new-signature-field
    BLANK-signed.pdf
    a blank file from elsewhere with an invisible signature
    
    Similarly to testChangeAppearances(), this test adds a signature
    appearance not breaking signature validity, but this time it contains
    the signer certificate subject common name.

--- src/test/java/mkl/testarea/itext7/signature/ChangeSignatureAppearance.java
+++ src/test/java/mkl/testarea/itext7/signature/ChangeSignatureAppearance.java
@@ -6,7 +6,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.security.Security;
+import java.security.cert.X509Certificate;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -22,6 +25,10 @@ import com.itextpdf.kernel.pdf.StampingProperties;
 import com.itextpdf.kernel.pdf.annot.PdfWidgetAnnotation;
 import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
 import com.itextpdf.kernel.pdf.xobject.PdfFormXObject;
+import com.itextpdf.layout.Canvas;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.signatures.CertificateInfo;
+import com.itextpdf.signatures.PdfPKCS7;
 import com.itextpdf.signatures.SignatureUtil;
 
 /**
@@ -35,6 +42,8 @@ public class ChangeSignatureAppearance
     public static void setUpBeforeClass() throws Exception
     {
         RESULT_FOLDER.mkdirs();
+        BouncyCastleProvider provider = new BouncyCastleProvider();
+        Security.addProvider(provider);
     }
 
     /**
@@ -83,4 +92,49 @@ public class ChangeSignatureAppearance
         }
     }
 
+    /**
+     * <a href="http://stackoverflow.com/questions/37027579/how-to-associate-a-previous-signature-in-a-new-signature-field">
+     * How to associate a previous signature in a new signature field
+     * </a>
+     * <br/>
+     * <span>BLANK-signed.pdf, <em>a blank file from elsewhere with an invisible signature.</em></span>
+     * <p>
+     * Similarly to {@link #testChangeAppearances()}, this test adds a signature appearance
+     * not breaking signature validity, but this time it contains the signer certificate
+     * subject common name.
+     * </p>
+     */
+    @Test
+    public void testChangeAppearancesWithName() throws IOException
+    {
+        try (   InputStream resource = getClass().getResourceAsStream("BLANK-signed.pdf");
+                PdfReader pdfReader = new PdfReader(resource);
+                OutputStream result = new FileOutputStream(new File(RESULT_FOLDER, "BLANK-signed-app-name.pdf"));
+                PdfWriter pdfWriter = new PdfWriter(result);
+                PdfDocument pdfDocument = new PdfDocument(pdfReader, pdfWriter, new StampingProperties().useAppendMode()))
+        {
+            SignatureUtil signatureUtil = new SignatureUtil(pdfDocument);
+            PdfAcroForm acroForm = PdfAcroForm.getAcroForm(pdfDocument, false);
+
+            for (String name : signatureUtil.getSignatureNames())
+            {
+                PdfPKCS7 pkcs7 = signatureUtil.verifySignature(name);
+                X509Certificate signerCert = (X509Certificate) pkcs7.getSigningCertificate();
+                String signerName = CertificateInfo.getSubjectFields(signerCert).getField("CN");
+                PdfFormField field = acroForm.getField(name);
+                field.setModified();
+                for (PdfWidgetAnnotation pdfWidgetAnnotation : field.getWidgets())
+                {
+                    pdfWidgetAnnotation.setRectangle(new PdfArray(new int[]{100, 100, 200, 200}));
+
+                    PdfFormXObject form = new PdfFormXObject(new Rectangle(100, 100));
+                    Canvas canvas = new Canvas(form, pdfDocument);
+                    canvas.add(new Paragraph().setItalic().add("Signed by:"));
+                    canvas.add(new Paragraph().setBold().add(signerName));
+
+                    pdfWidgetAnnotation.setNormalAppearance(form.getPdfObject());
+                }
+            }
+        }
+    }
 }
