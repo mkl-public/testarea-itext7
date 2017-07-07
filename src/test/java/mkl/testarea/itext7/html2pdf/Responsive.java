741ad2b2c434b8969e627cde73ae246fe982e6b6
Michael Klink <Michael.Klink@exceet.de>
2017-07-07 09:24:29 +0200

    pdfHTML: Responsive Design from iText web site
    As the links on the iText web site for the pdfHTML: Responsive Design example <http://developers.itextpdf.com/content/itext-7-examples/converting-html-pdf/pdfhtml-responsive-design> don't work anymore and the files could not be found on <https://git.itextsupport.com/>, here an adapted version.
    
    It can be run with iText 7.0.2 and html2pdf 1.0.0.

---
+++ src/test/java/mkl/testarea/itext7/html2pdf/Responsive.java
@@ -0,0 +1,89 @@
+/**
+ * 
+ */
+package mkl.testarea.itext7.html2pdf;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.itextpdf.html2pdf.ConverterProperties;
+import com.itextpdf.html2pdf.HtmlConverter;
+import com.itextpdf.html2pdf.css.media.MediaDeviceDescription;
+import com.itextpdf.html2pdf.css.media.MediaType;
+import com.itextpdf.html2pdf.css.util.CssUtils;
+import com.itextpdf.kernel.geom.PageSize;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.font.FontProvider;
+import com.itextpdf.licensekey.LicenseKey;
+
+/**
+ * @author mklink
+ *
+ */
+public class Responsive
+{
+    final static File RESULT_FOLDER = new File("target/test-outputs", "html2pdf");
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception
+    {
+        RESULT_FOLDER.mkdirs();
+    }
+
+    @Test
+    public void testResponsiveDesign() throws IOException
+    {
+        LicenseKey.loadLicenseFile("itextkey-html2pdf_typography.xml");
+
+        String resourceFolder = "src/test/resources/mkl/testarea/itext7/html2pdf/";
+        PageSize[] pageSizes = {
+                PageSize.A4.rotate(),
+                new PageSize(720, PageSize.A4.getHeight()),
+                new PageSize(PageSize.A5.getWidth(), PageSize.A4.getHeight())
+        };
+
+        for (int i = 0; i < pageSizes.length; i++) {
+            float width = CssUtils.parseAbsoluteLength("" + pageSizes[i].getWidth());
+            File dest = new File(RESULT_FOLDER, String.format("responsive_%s.pdf", width));
+            try ( InputStream htmlSource = getClass().getResourceAsStream("responsive.html")    )
+            {
+                parseMedia(htmlSource, dest, resourceFolder, pageSizes[i], width);
+            }
+        }
+    }
+
+    public void parseMedia(InputStream htmlSource, File pdfDest, String resourceLoc, PageSize pageSize, float screenWidth) throws IOException {
+        PdfWriter writer = new PdfWriter(pdfDest.getAbsolutePath());
+        PdfDocument pdfDoc = new PdfDocument(writer);
+ 
+        //Set the result to be tagged
+        pdfDoc.setTagged();
+        pdfDoc.setDefaultPageSize(pageSize);
+ 
+        ConverterProperties converterProperties = new ConverterProperties();
+ 
+        //Set media device description details
+        MediaDeviceDescription mediaDescription = new MediaDeviceDescription(MediaType.SCREEN);
+        mediaDescription.setWidth(screenWidth);
+        converterProperties.setMediaDeviceDescription(mediaDescription);
+ 
+        FontProvider fp = new FontProvider();
+        fp.addStandardPdfFonts();
+        //Register external font directory
+        fp.addDirectory(resourceLoc);
+ 
+        converterProperties.setFontProvider(fp);
+        converterProperties.setBaseUri(resourceLoc);
+ 
+        //Create acroforms from text and button input fields
+        //converterProperties.setCreateAcroForm(true);
+ 
+        HtmlConverter.convertToPdf(htmlSource, pdfDoc, converterProperties);
+        pdfDoc.close();
+    }
+}
