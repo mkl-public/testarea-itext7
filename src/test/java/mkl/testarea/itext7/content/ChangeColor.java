98d6c817b5885b4d7339ad6595021e6e41463c3b
mkl <mkl@wir-sind-cool.org>
2019-09-19 17:14:12 +0200

    New ChangeColor test focusing on the stack overflow question
    Traverse whole PDF and change blue color to black ( Change color of
    underlines as well) + iText
    https://stackoverflow.com/questions/57993761/traverse-whole-pdf-and-change-blue-color-to-black-change-color-of-underlines-a
    originalFile.pdf
    https://raad-dev-test.s3.ap-south-1.amazonaws.com/36/2019-08-30/originalFile.pdf
    
    testChangeFillRgbBlueToBlack - This canvas editor is related to the
    editor in EditPageContent.testChangeBlackTextToGreenDocument(). In
    contrast to that editor, though, the editor here shall change the fill
    color of arbitrary content, not only text content. Thus, this actually
    is simpler than the original.

---
+++ src/test/java/mkl/testarea/itext7/content/ChangeColor.java
@@ -0,0 +1,89 @@
+package mkl.testarea.itext7.content;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfLiteral;
+import com.itextpdf.kernel.pdf.PdfNumber;
+import com.itextpdf.kernel.pdf.PdfObject;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfCanvasProcessor;
+
+/**
+ * @author mkl
+ */
+public class ChangeColor {
+    final static File RESULT_FOLDER = new File("target/test-outputs", "content");
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        RESULT_FOLDER.mkdirs();
+    }
+
+    /**
+     * <a href="https://stackoverflow.com/questions/57993761/traverse-whole-pdf-and-change-blue-color-to-black-change-color-of-underlines-a">
+     * Traverse whole PDF and change blue color to black ( Change color of underlines as well) + iText
+     * </a>
+     * <br/>
+     * <a href="https://raad-dev-test.s3.ap-south-1.amazonaws.com/36/2019-08-30/originalFile.pdf">
+     * originalFile.pdf
+     * </a>
+     * <p>
+     * This canvas editor is related to the editor in
+     * {@link EditPageContent#testChangeBlackTextToGreenDocument()}.
+     * In contrast to that editor, though, the editor here shall
+     * change the fill color of arbitrary content, not only text
+     * content. Thus, this actually is simpler than the original.
+     * </p>
+     */
+    @Test
+    public void testChangeFillRgbBlueToBlack() throws IOException {
+        try (   InputStream resource = getClass().getResourceAsStream("originalFile.pdf");
+                PdfReader pdfReader = new PdfReader(resource);
+                OutputStream result = new FileOutputStream(new File(RESULT_FOLDER, "originalFile-FillRgbBlueToBlack.pdf"));
+                PdfWriter pdfWriter = new PdfWriter(result);
+                PdfDocument pdfDocument = new PdfDocument(pdfReader, pdfWriter) )
+        {
+            PdfCanvasEditor editor = new PdfCanvasEditor()
+            {
+                @Override
+                protected void write(PdfCanvasProcessor processor, PdfLiteral operator, List<PdfObject> operands)
+                {
+                    String operatorString = operator.toString();
+
+                    if (SET_FILL_RGB.equals(operatorString) && operands.size() == 4) {
+                        if (isApproximatelyEqual(operands.get(0), 0) &&
+                                isApproximatelyEqual(operands.get(1), 0) &&
+                                isApproximatelyEqual(operands.get(2), 1)) {
+                            super.write(processor, new PdfLiteral("g"), Arrays.asList(new PdfNumber(0), new PdfLiteral("g")));
+                            return;
+                        }
+                    }
+                    
+                    super.write(processor, operator, operands);
+                }
+
+                boolean isApproximatelyEqual(PdfObject number, float reference) {
+                    return number instanceof PdfNumber && Math.abs(reference - ((PdfNumber)number).floatValue()) < 0.01f;
+                }
+
+                final String SET_FILL_RGB = "rg";
+            };
+            for (int i = 1; i <= pdfDocument.getNumberOfPages(); i++)
+            {
+                editor.editPage(pdfDocument, i);
+            }
+        }
+    }
+
+}
