fc86ba44f439b5cc8c3b35233ebe79a5e32cf89a
mkl <mkl@wir-sind-cool.org>
2019-11-04 21:14:27 +0100

    New ShowInfoMetadata tests focusing on the stack overflow question
    iText 7.1.8 can't read some pdf file
    https://stackoverflow.com/questions/58659870/itext-7-1-8-cant-read-some-pdf-file
    x001.pdf
    https://drive.google.com/open?id=1AVda9qst-DiVbz4QFQh8iIv2aLn-uXRR
    
    testX001 - The PDF here is processed in 81 seconds. In particular it's
    not stuck, and its also not a 12 minutes wait here in Java.

--- src/test/java/mkl/testarea/itext7/meta/ShowInfoMetadata.java
+++ src/test/java/mkl/testarea/itext7/meta/ShowInfoMetadata.java
@@ -37,4 +37,30 @@ public class ShowInfoMetadata {
         for (PdfName key : infoDictionary.keySet())
             System.out.printf("* %s -> %s\n", key, infoDictionary.getAsString(key));
     }
+
+    /**
+     * <a href="https://stackoverflow.com/questions/58659870/itext-7-1-8-cant-read-some-pdf-file">
+     * iText 7.1.8 can't read some pdf file
+     * </a>
+     * <br/>
+     * <a href="https://drive.google.com/open?id=1AVda9qst-DiVbz4QFQh8iIv2aLn-uXRR">
+     * x001.pdf
+     * </a>
+     * <p>
+     * The PDF here is processed in 81 seconds. In particular it's
+     * not stuck, and its also not a 12 minutes wait here in Java.
+     * </p>
+     */
+    @Test
+    public void testX001() throws IOException {
+        System.out.printf("\n%s\n", "x001.pdf");
+        long start = System.currentTimeMillis();
+        try (   InputStream resource = getClass().getResourceAsStream("x001.pdf");
+                PdfReader pdfReader = new PdfReader(resource);
+                PdfDocument pdfDocument = new PdfDocument(pdfReader)) {
+            showMetadata(pdfDocument);
+        }
+        long end = System.currentTimeMillis();
+        System.out.printf("- test took %s seconds\n", (end-start)/1000f);
+    }
 }
