c35148203c6aab0ec55fd994a42efa1d10ed00cf
mkl <klink@csi.com>
2021-10-19 17:41:40 +0200

    New CreateLink test focusing on the stack overflow question 69557142
    How do I create a forward link to a specific page which does not yet exists when I generate the link
    https://stackoverflow.com/questions/69557142/how-do-i-create-a-forward-link-to-a-specific-page-which-does-not-yet-exists-when
    
    testCreateForwardLink - This test demonstrates how to create a link to a not yet created page by means of named destinations.

--- src/test/java/mkl/testarea/itext7/annotate/CreateLink.java
+++ src/test/java/mkl/testarea/itext7/annotate/CreateLink.java
@@ -11,11 +11,15 @@ import org.junit.Test;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.kernel.pdf.action.PdfAction;
+import com.itextpdf.kernel.pdf.navigation.PdfExplicitDestination;
+import com.itextpdf.kernel.pdf.navigation.PdfStringDestination;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.AreaBreak;
 import com.itextpdf.layout.element.Cell;
 import com.itextpdf.layout.element.Link;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
+import com.itextpdf.layout.properties.AreaBreakType;
 
 /**
  * @author mkl
@@ -56,6 +60,39 @@ public class CreateLink
 
             document.add(table);
             document.close();
+        }
+    }
+
+    /**
+     * <a href="https://stackoverflow.com/questions/69557142/how-do-i-create-a-forward-link-to-a-specific-page-which-does-not-yet-exists-when">
+     * How do I create a forward link to a specific page which does not yet exists when I generate the link
+     * </a>
+     * <p>
+     * This test demonstrates how to create a link to a not yet created page
+     * by means of named destinations.
+     * </p>
+     */
+    @Test
+    public void testCreateForwardLink() throws IOException {
+        try (   FileOutputStream fos = new FileOutputStream(new File(RESULT_FOLDER, "forwardLink.pdf"));
+                PdfDocument pdfDocument = new PdfDocument(new PdfWriter(fos));
+                Document document = new Document(pdfDocument)   )
+        {
+            String destinationName = "MyForwardDestination";
+
+            for (int page = 1; page <= 50; page++) {
+                document.add(new Paragraph().setFontSize(100).add(String.valueOf(page)));
+                switch (page) {
+                case 1:
+                    document.add(new Paragraph(new Link("Click here for a forward jump", new PdfStringDestination(destinationName)).setFontSize(20)));
+                    break;
+                case 42:
+                    pdfDocument.addNamedDestination(destinationName, PdfExplicitDestination.createFit(pdfDocument.getLastPage()).getPdfObject());
+                    break;
+                }
+                if (page < 50)
+                    document.add(new AreaBreak(AreaBreakType.NEXT_PAGE));
+            }
         }
     }
 }
